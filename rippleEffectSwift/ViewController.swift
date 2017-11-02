9bfa0d27638324343774c9f63f7686f66d3f559a
anoopm <anoop_m@infosys.com>
2017-11-03 00:48:09 +0530

    Ripple effect

---
+++ rippleEffectSwift/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  rippleEffectSwift
+//
+//  Created by anoopm on 03/11/17.
+//  Copyright © 2017 anoopm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var imageView:UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animateImage()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func animateImage() {
+        addRippleEffect(to: imageView)
+    }
+    
+    @IBAction func animateButton(_ sender: UIButton) {
+        addRippleEffect(to: sender)
+    }
+    
+    func addRippleEffect(to referenceView: UIView) {
+        /*! Creates a circular path around the view*/
+        let path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: referenceView.bounds.size.width, height: referenceView.bounds.size.height))
+        /*! Position where the shape layer should be */
+        let shapePosition = CGPoint(x: referenceView.bounds.size.width / 2.0, y: referenceView.bounds.size.height / 2.0)
+        let rippleShape = CAShapeLayer()
+        rippleShape.bounds = CGRect(x: 0, y: 0, width: referenceView.bounds.size.width, height: referenceView.bounds.size.height)
+        rippleShape.path = path.cgPath
+        rippleShape.fillColor = UIColor.clear.cgColor
+        rippleShape.strokeColor = UIColor.yellow.cgColor
+        rippleShape.lineWidth = 4
+        rippleShape.position = shapePosition
+        rippleShape.opacity = 0
+        
+        /*! Add the ripple layer as the sublayer of the reference view */
+        referenceView.layer.addSublayer(rippleShape)
+        /*! Create scale animation of the ripples */
+        let scaleAnim = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnim.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+        scaleAnim.toValue = NSValue(caTransform3D: CATransform3DMakeScale(2, 2, 1))
+        /*! Create animation for opacity of the ripples */
+        let opacityAnim = CABasicAnimation(keyPath: "opacity")
+        opacityAnim.fromValue = 1
+        opacityAnim.toValue = nil
+        /*! Group the opacity and scale animations */
+        let animation = CAAnimationGroup()
+        animation.animations = [scaleAnim, opacityAnim]
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animation.duration = CFTimeInterval(0.7)
+        animation.repeatCount = 25
+        animation.isRemovedOnCompletion = true
+        rippleShape.add(animation, forKey: "rippleEffect")
+    }
+
+}
+
